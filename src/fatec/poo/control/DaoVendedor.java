9362bc58d6888df98df54c76c2bdebfef46552fe
Caique Souza <caique.siqueira@fatec.sp.gov.br>
2017-11-27 16:18:39 -0200

    Fixes SQL commands and parameters It's TP_Vendedor, not Vendedor.

--- src/fatec/poo/control/DaoVendedor.java
+++ src/fatec/poo/control/DaoVendedor.java
@@ -16,17 +16,21 @@ public class DaoVendedor {
     public void inserir(Vendedor vendedor){
         PreparedStatement ps = null;
         try{
-            ps = conn.prepareStatement("INSERT INTO Vendedor VALUES(?,?,?,?,?,?,?,?,?,?)");
-            ps.setString(0, vendedor.getCpf());
-            ps.setString(1, vendedor.getNome());
-            ps.setString(2, vendedor.getEndereco());
-            ps.setString(3, vendedor.getCidade());
-            ps.setString(4, vendedor.getCep());
-            ps.setString(5, vendedor.getUf());
-            ps.setString(6, vendedor.getDdd());
-            ps.setString(7, vendedor.getTelefone());
-            ps.setDouble(8, vendedor.getSalarioBase());
-            ps.setDouble(9, vendedor.getComissao());
+            ps = conn.prepareStatement(
+                "INSERT INTO TP_Vendedor" +
+                " (cpf,nome,endereco,cidade,cep,uf,ddd,telefone,salario_base,comissao)" +
+                " VALUES (?,?,?,?,?,?,?,?,?,?)"
+            );
+            ps.setString(1, vendedor.getCpf());
+            ps.setString(2, vendedor.getNome());
+            ps.setString(3, vendedor.getEndereco());
+            ps.setString(4, vendedor.getCidade());
+            ps.setString(5, vendedor.getCep());
+            ps.setString(6, vendedor.getUf());
+            ps.setString(7, vendedor.getDdd());
+            ps.setString(8, vendedor.getTelefone());
+            ps.setDouble(9, vendedor.getSalarioBase());
+            ps.setDouble(10, vendedor.getComissao());
             ps.execute();
         }
         catch(SQLException ex){
@@ -39,18 +43,20 @@ public class DaoVendedor {
         PreparedStatement ps = null;
         
         try{
-            ps = conn.prepareStatement("UPDATE Vendedor SET nome = ?, endereco = ?,cidade = ?,cep = ?, uf = ?, ddd= ?, telefone = ?, sal_base = ?, comissao = ? WHERE cpf = ?");
-            ps.setString(1, vendedor.getNome());
-            ps.setString(2, vendedor.getEndereco());
-            ps.setString(3, vendedor.getCidade());
-            ps.setString(4, vendedor.getCep());
-            ps.setString(5, vendedor.getUf());
-            ps.setString(6, vendedor.getDdd());
-            ps.setString(7, vendedor.getTelefone());
-            ps.setDouble(8, vendedor.getSalarioBase());
-            ps.setDouble(9, vendedor.getComissao());
-            
-            ps.setString(10, vendedor.getCpf());
+            ps = conn.prepareStatement(
+                "UPDATE TP_Vendedor" +
+                " SET endereco = ?,cidade = ?,cep = ?, uf = ?, ddd= ?, telefone = ?, salario_base = ?, comissao = ?" +
+                " WHERE cpf = ?"
+            );
+            ps.setString(1, vendedor.getEndereco());
+            ps.setString(2, vendedor.getCidade());
+            ps.setString(3, vendedor.getCep());
+            ps.setString(4, vendedor.getUf());
+            ps.setString(5, vendedor.getDdd());
+            ps.setString(6, vendedor.getTelefone());
+            ps.setDouble(7, vendedor.getSalarioBase());
+            ps.setDouble(8, vendedor.getComissao());
+            ps.setString(9, vendedor.getCpf());
             ps.execute();
         }
         catch(SQLException ex){
@@ -59,34 +65,37 @@ public class DaoVendedor {
     }
     
     public Vendedor consultar(String cpf){
-        Vendedor ven = null;
+        Vendedor vendedor = null;
         PreparedStatement ps = null;
         try{
-            ps = conn.prepareStatement("SELECT * FROM Vendedor WHERE cpf = ?");
+            ps = conn.prepareStatement("SELECT * FROM TP_Vendedor WHERE cpf = ?");
             ps.setString(1, cpf);
             ResultSet rs = ps.executeQuery();
             
             if (rs.next() == true) {
-                ven = new Vendedor (cpf, rs.getString("nome"), rs.getDouble("Sal_base"));
-                ven.setCep(rs.getString("cep"));
-                ven.setCidade(rs.getString("cidade"));
-                ven.setDdd(rs.getString("ddd"));
-                ven.setEndereco(rs.getString("endereco"));
-                ven.setTelefone(rs.getString("telefone"));
-                ven.setUf(rs.getString("uf"));
-                ven.setComissao(rs.getDouble("Comissao"));
+                System.out.println("Achou vendedor CPF " + cpf);
+                vendedor = new Vendedor (cpf, rs.getString("nome"), rs.getDouble("Salario_base"));
+                vendedor.setCep(rs.getString("cep"));
+                vendedor.setCidade(rs.getString("cidade"));
+                vendedor.setDdd(rs.getString("ddd"));
+                vendedor.setEndereco(rs.getString("endereco"));
+                vendedor.setTelefone(rs.getString("telefone"));
+                vendedor.setUf(rs.getString("uf"));
+                vendedor.setComissao(rs.getDouble("Comissao"));
+            } else {
+                System.out.println("Vendedor CPF " + cpf + "não encontrado");
             }
         }
         catch(SQLException ex){
             System.out.println(ex.toString());
         }
-        return ven;
+        return vendedor;
     }
     
     public void excluir(Vendedor vendedor){
         PreparedStatement ps = null;
         try{
-            ps = conn.prepareStatement("DELETE FROM Vendedor WHERE = ?");
+            ps = conn.prepareStatement("DELETE FROM TP_Vendedor WHERE = ?");
             ps.setString(1, vendedor.getCpf());
             
             ps.execute();
